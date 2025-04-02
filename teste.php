c24e49e925ae29c5506255f933eb3aeaa48d28dc
Andrei Mendes <60723336+ArcMendesCD@users.noreply.github.com>
2025-04-02 14:57:45 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connection = mysqli_connect("localhost", "antaresadm","2024#antaresADM0403","antaresdb");
-
-if(!$connection)
-    die("could not connect".mysqli_connect_error());
-else
- echo 'Connection estabilished';
-?>
